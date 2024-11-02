4fd5f4d5eddc303e5fdf32be01fb9912f4e4dd83
Premysl Bednarek <premik09@seznam.cz>
2024-11-02 12:14:17 +0100

    feat: Add ReportModule resource with CRUD operations

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ReportModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/report/report.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateReportDto } from './dto/create-report.dto';
+import { UpdateReportDto } from './dto/update-report.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ReportService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(createReportDto: CreateReportDto) {
+    return 'This action adds a new report';
+  }
+
+  findAll() {
+    return `This action returns all report`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} report`;
+  }
+
+  update(id: number, updateReportDto: UpdateReportDto) {
+    return `This action updates a #${id} report`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} report`;
+  }
+}

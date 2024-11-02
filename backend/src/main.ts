560458028c045c428271ea34fa4183aa073ef265
Premysl Bednarek <premik09@seznam.cz>
2024-11-02 23:26:43 +0100

    feat: Add json openapi spec to docs-json path

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,9 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('docs', app, document, {
+    jsonDocumentUrl: 'docs-json',
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }

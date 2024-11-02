36fcf2a2285f6d89896a7d03a30f1f8f13d55461
Premysl Bednarek <premik09@seznam.cz>
2024-11-02 23:06:59 +0100

    Remove unnecessary files from nest project generating

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ReportModule } from './report/report.module';
 import { FileModule } from './file/file.module';
 
 @Module({
   imports: [PrismaModule, ReportModule, FileModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

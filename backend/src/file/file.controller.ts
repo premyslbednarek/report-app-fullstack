b11c2ec24559140649f4ad210d615a85b552f8c8
Premysl Bednarek <premik09@seznam.cz>
2024-11-02 22:33:28 +0100

    feat: Add file download functionality

--- backend/src/file/file.controller.ts
+++ backend/src/file/file.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, StreamableFile } from '@nestjs/common';
 import { FileService } from './file.service';
+import { createReadStream } from 'fs';
+import { join } from 'path';
 
 @Controller('file')
 export class FileController {
   constructor(private readonly fileService: FileService) {}
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.fileService.findOne(id);
+  async getFile(@Param('id') id: string) {
+    const fileEntity = await this.fileService.findOne(id);
+    const file = createReadStream(join('uploads', fileEntity.diskName));
+    return new StreamableFile(file, {
+      disposition: `attachment; filename="${fileEntity.name}"`,
+    });
   }
 }

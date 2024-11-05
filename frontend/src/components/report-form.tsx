cb0d5dc992088b22d1f4016669ff6a4d9137778b
Premysl Bednarek <premik09@seznam.cz>
2024-11-05 21:28:36 +0100

    refactor: move new report form submit login out of the Form

--- frontend/src/components/report-form.tsx
+++ frontend/src/components/report-form.tsx
@@ -1,7 +1,5 @@
-import { CreateReportWithFilesDto, ReportService } from "@/client";
 import { useState } from "react";
 import { Button } from "./ui/button";
-import { useToast } from "@/hooks/use-toast";
 import { Loader2 } from "lucide-react";
 import { FormProvider, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -9,7 +7,7 @@ import FormInput from "./form/form-input";
 import FormTextarea from "./form/form-textarea";
 import { z } from "zod";
 
-const createReportSchema = z.object({
+export const createReportSchema = z.object({
   title: z.string().min(1, { message: "Title is required" }),
   description: z.string().min(1, { message: "Description is required" }),
   authorName: z.string(),
@@ -20,32 +18,30 @@ const createReportSchema = z.object({
   files: z.instanceof(FileList).transform((files) => Array.from(files)),
 });
 
-type CreateReportSchema = z.infer<typeof createReportSchema>;
+export type CreateReportSchema = z.infer<typeof createReportSchema>;
 
-const CreateReportForm = () => {
-  const { toast } = useToast(); // show toasts - submit success/error
+type FormProps = {
+  onFormSubmit: (formData: CreateReportSchema) => Promise<void>;
+} & React.FormHTMLAttributes<HTMLFormElement>;
+
+const CreateReportForm = ({ onFormSubmit, ...props }: FormProps) => {
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const form = useForm<CreateReportSchema>({
     resolver: zodResolver(createReportSchema),
   });
 
-  const onSubmit = async (data: CreateReportWithFilesDto) => {
-    try {
-      setIsSubmitting(true);
-      await ReportService.reportControllerCreate(data);
-      toast({ description: "Report created successfully!" });
-      setIsSubmitting(false);
-      form.reset();
-    } catch {
-      toast({ description: "There was an error creating the report" });
-    }
+  const onSubmit = async (formData: CreateReportSchema) => {
+    setIsSubmitting(true);
+    await onFormSubmit(formData);
+    setIsSubmitting(false);
   };
 
   return (
     <FormProvider {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
+        {...props}
         className="max-w-lg mx-auto p-5 shadow-md rounded-md space-y-2 border border-gray-300"
       >
         <FormInput
@@ -72,17 +68,9 @@ const CreateReportForm = () => {
         />
         <FormInput label="Upload Files" name="files" type="file" multiple />
 
-        <div>
-          {isSubmitting ? (
-            <Button type="submit" className="w-full" disabled>
-              <Loader2 /> Creating Report...
-            </Button>
-          ) : (
-            <Button type="submit" className="w-full">
-              Create Report
-            </Button>
-          )}
-        </div>
+        <Button type="submit" className="w-full" disabled={isSubmitting}>
+          {isSubmitting ? <Loader2 /> : "Submit"}
+        </Button>
       </form>
     </FormProvider>
   );
--- frontend/src/routes/new-report.tsx
+++ frontend/src/routes/new-report.tsx
@@ -1,7 +1,20 @@
-import CreateReportForm from "@/components/report-form";
+import { ReportService } from "@/client";
+import CreateReportForm, { CreateReportSchema } from "@/components/report-form";
+import { useToast } from "@/hooks/use-toast";
 
 const NewReport = () => {
-  return <CreateReportForm />;
+  const { toast } = useToast(); // show toasts - submit success/error
+
+  const onSubmit = async (formData: CreateReportSchema) => {
+    try {
+      await ReportService.reportControllerCreate(formData);
+      toast({ description: "Report created successfully!" });
+    } catch {
+      toast({ description: "There was an error creating the report" });
+    }
+  };
+
+  return <CreateReportForm onFormSubmit={onSubmit} />;
 };
 
 export default NewReport;

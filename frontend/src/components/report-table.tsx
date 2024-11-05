bfd34081f971348019d5588dcbd6f0d9e88e3c89
Premysl Bednarek <premik09@seznam.cz>
2024-11-05 22:42:39 +0100

    feat: sort reports by update time

--- frontend/src/components/report-table.tsx
+++ frontend/src/components/report-table.tsx
@@ -14,6 +14,7 @@ import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
+  getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 import ReportDetailsModal from "./report-details-modal";
@@ -74,6 +75,10 @@ const ReportTable = () => {
     data: data || [],
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    initialState: {
+      sorting: [{ id: "updatedAt", desc: true }],
+    },
   });
 
   if (isLoading) {
